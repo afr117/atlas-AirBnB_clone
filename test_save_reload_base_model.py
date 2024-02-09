8024105266505a34cfd23752cd0c88eb13b72de6
afr117 <alfredo.figueroa07@yahoo.com>
2024-02-09 18:06:08 +0000

    test output limit

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-Test script to demonstrate saving and reloading objects using BaseModel and FileStorage.
-"""
-
 from models import storage
 from models.base_model import BaseModel
 
-# Reload objects from the file, if any
+all_objs = storage.all()
 print("-- Reloaded objects --")
-for obj in storage.all().values():
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
     print(obj)
 
-# Create a new object
 print("-- Create a new object --")
 my_model = BaseModel()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
 my_model.save()
 print(my_model)
-
