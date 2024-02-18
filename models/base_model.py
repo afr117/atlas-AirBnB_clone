21ce8011581023bfa42ab9b7162c0fb62176b717
afr117 <54492982+afr117@users.noreply.github.com>
2024-02-18 13:59:33 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,10 @@
-#!/usr/bin/python3
-"""
-This module defines the BaseModel class.
-"""
-import uuid
-from datetime import datetime
+from models.__init__ import storage
 
 class BaseModel:
-    """
-    BaseModel class for common attributes/methods for other classes.
-    """
+    """BaseModel class for common attributes/methods for other classes."""
+
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new BaseModel instance.
-        """
+        """Initializes a new BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -24,21 +16,15 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             # Call new method of storage to add the new object
-            from models import storage
             storage.new(self)
 
     def save(self):
-        """
-        Updates the updated_at attribute with the current datetime.
-        """
-        from models.engine import storage
+        """Updates the updated_at attribute with the current datetime and saves."""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary representation of the object.
-        """
+        """Returns a dictionary representation of the object."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         if 'created_at' in obj_dict:
@@ -46,17 +32,3 @@ class BaseModel:
         if 'updated_at' in obj_dict:
             obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-    def __str__(self):
-        """
-        Returns the string representation of the object.
-        """
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
-
-    def __repr__(self):
-        """
-        Returns the string representation of the object.
-        """
-        return str(self.to_dict())
-
