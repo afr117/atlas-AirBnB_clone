3117cd3a1f6ea91646cd382b3646fbe4e2f09f6c
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-04 19:53:32 +0000

    test base

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
@@ -9,16 +8,16 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
             if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs
-                        ['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                created_at_str = kwargs['created_at']
+                self.created_at = datetime.strptime(created_at_str, "%Y-%m-%dT%H:%M:%S.%f")
             if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs
-                        ['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                updated_at_str = kwargs['updated_at']
+                self.updated_at = datetime.strptime(updated_at_str, "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+    
     def save(self):
         self.updated_at = datetime.now()
         # Add code to save to file storage
