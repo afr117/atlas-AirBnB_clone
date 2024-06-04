5a2b4ac095d65222481b1ce86f714ffd0d12bbd4
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-04 19:26:10 +0000

    update pycode basemode

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
@@ -8,9 +9,11 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
             if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                self.created_at = datetime.strptime(kwargs['created_at'], 
+                        "%Y-%m-%dT%H:%M:%S.%f")
             if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                self.updated_at = datetime.strptime(kwargs['updated_at'],
+                        "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -26,4 +29,3 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
-
