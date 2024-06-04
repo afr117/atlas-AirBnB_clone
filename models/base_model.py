8fb1839848c8aa731c449c13a0d4dba5b089cddd
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-04 19:57:44 +0000

    test base

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
@@ -9,15 +10,19 @@ class BaseModel:
                     setattr(self, key, value)
             if 'created_at' in kwargs:
                 created_at_str = kwargs['created_at']
-                self.created_at = datetime.strptime(created_at_str, "%Y-%m-%dT%H:%M:%S.%f")
+                self.created_at = datetime.strptime(
+                    created_at_str, "%Y-%m-%dT%H:%M:%S.%f"
+                )
             if 'updated_at' in kwargs:
                 updated_at_str = kwargs['updated_at']
-                self.updated_at = datetime.strptime(updated_at_str, "%Y-%m-%dT%H:%M:%S.%f")
+                self.updated_at = datetime.strptime(
+                    updated_at_str, "%Y-%m-%dT%H:%M:%S.%f"
+                )
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+
     def save(self):
         self.updated_at = datetime.now()
         # Add code to save to file storage
@@ -28,3 +33,4 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
