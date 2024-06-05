e8f3640b67fb94c5727eb5811296d3db43ca8794
afr117 <alfredo.figueroa07@yahoo.com>
2024-06-05 13:36:10 +0000

    t 5 models not found

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,33 @@
+# models/base_model.py
+
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
                     setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def save(self):
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,5 @@
+# models/engine/file_storage.py
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -7,28 +9,32 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
+        """Deserializes the JSON file to __objects (if it exists)"""
         try:
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
-            for key, value in obj_dict.items():
-                class_name = value['__class__']
-                self.__objects[key] = eval(class_name)(**value)
+            for key, obj_data in obj_dict.items():
+                self.__objects[key] = BaseModel(**obj_data)
         except FileNotFoundError:
             pass
+
